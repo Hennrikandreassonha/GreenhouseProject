624683fff420c4309df168367ac148ea6e1ae3ed
Henrik1995a@live.se <henrik1995a@live.se>
2023-07-02 08:31:49 +0200

    123

--- light.py
+++
@@ -1,8 +0,0 @@
-from lib.tsl2591 import TSL2591
-import machine
-i2clight = machine.I2C(0, sda=machine.Pin(16), scl=machine.Pin(17), freq=400000)
-
-tsl = TSL2591(i2clight)  # initialize
-lux = tsl.get_lux()
-
-print(lux)
